4d51b9e334141271a248451dae7e9b7bf0038586
Newbluewood <157754944+Newbluewood@users.noreply.github.com>
2024-07-24 10:10:08 +0200

    Add files via upload

---
+++ src/components/MainNav.vue
@@ -0,0 +1,97 @@
+<script setup>
+import { ref } from 'vue'
+import AppLink from './AppLink.vue'
+const IsVisible = ref(false)
+
+function hideNawbar() {
+  IsVisible.value = !IsVisible.value
+}
+</script>
+
+<template>
+  <div class="navWrapper">
+    <div class="navBurger icon-th-list" @click="hideNawbar()" v-show="!IsVisible"></div>
+    <div class="navBurger icon-cancel" @click="hideNawbar()" v-show="IsVisible"></div>
+    <div class="nav mobile" v-show="IsVisible">
+      <AppLink to="/">Home</AppLink>
+      <AppLink to="/about">About</AppLink>
+      <AppLink to="/quiz">Quiz</AppLink>
+      <AppLink to="/board">Board</AppLink>
+      <AppLink to="https://quizapi.io/api-config">API</AppLink>
+      <div class="dots">. . .</div>
+      <AppLink to="/Login">Login</AppLink>
+    </div>
+    <div class="nav wide">
+      <AppLink to="/">Home</AppLink>
+      <AppLink to="/about">About</AppLink>
+      <AppLink to="/quiz">Quiz</AppLink>
+      <AppLink to="/board">Board</AppLink>
+      <AppLink to="https://quizapi.io/api-config">API</AppLink>
+      <div class="dots">. . .</div>
+      <AppLink to="/Login">Login</AppLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.nav {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+  padding: 0.8rem 1rem;
+}
+.nav > a {
+  border: 1px solid black;
+  padding: 0.4rem 2rem;
+  text-align: center;
+  margin: 2px;
+  width: 100%;
+  font-size: larger;
+}
+.navBurger {
+  font-size: 1.5rem;
+  padding: 0 0.5rem;
+}
+@media (min-width: 1024px) {
+}
+@media (min-width: 741px) {
+  .navBurger {
+    display: none;
+  }
+  .nav {
+    justify-content: space-around;
+    flex-direction: row;
+    border: none;
+    padding: 0;
+    width: 200px;
+  }
+  .nav > a {
+    font-size: 1.5rem;
+
+    border: none;
+  }
+  .nav.mobile {
+    display: none;
+  }
+  .navWrapper {
+    display: flex;
+    align-items: center;
+  }
+  .dots {
+    display: none;
+  }
+}
+@media (max-width: 740px) {
+  .nav.wide {
+    display: none;
+  }
+}
+@media (max-width: 380px) {
+  .nav.wide {
+    display: none;
+  }
+}
+</style>
---
+++ src/components/SwitchButton.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref } from 'vue'
+
+let isDark = ref(false)
+
+function switchButon() {
+  isDark.value = !isDark.value
+  if (isDark.value) {
+    document.querySelector(':root').classList.add('dark')
+  } else {
+    document.querySelector(':root').classList.remove('dark')
+  }
+}
+</script>
+
+<template>
+  <i class="icons icon-sun"></i>
+  <div class="SwitchButton" @click="switchButon()">
+    <div class="circle" :class="{ isOn: isDark }"></div>
+  </div>
+  <i class="icons icon-moon"></i>
+</template>
+
+<style scoped>
+.SwitchButton {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  cursor: pointer;
+  margin: 0.1rem;
+  border: 1px solid var(--color-border);
+  border-radius: 1rem;
+  height: 1rem;
+  width: 2.2rem;
+  background-color: var(--color-background-mute);
+  position: relative;
+}
+.icons {
+  font-size: 1.2rem;
+}
+.circle {
+  border: 1px solid var(--color-border);
+  border-radius: 1rem;
+  height: 1.2rem;
+  width: 1.2rem;
+  background-color: var(--color-text);
+  position: absolute;
+  left: -0.1rem;
+  transition: left 0.2s ease-in-out;
+}
+.circle.isOn {
+  border: 1px solid var(--color-border);
+  border-radius: 1rem;
+  height: 1.2rem;
+  width: 1.2rem;
+  background-color: var(--color-text);
+  position: absolute;
+  left: 1rem;
+  transition: left 0.2s ease-in-out;
+}
+</style>
